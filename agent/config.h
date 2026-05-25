18a015e92edb442f79a4ed914ab5f1278c517f1c
Dokka01 <106928889+Dokka01@users.noreply.github.com>
2026-05-26 01:15:01 +0200

    chore(agent): remove unused logs module, update config.h

--- agent/config.h
+++ agent/config.h
@@ -1,10 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define SERVER_URL   "http://127.0.0.1:5000/api/machines/report"
-#define LOGS_DIR     "C:\\Windows\\Logs"
-#define SCREENSHOT   "screenshot.bmp"
-#define BUFFER_SIZE  256
-#define DATA_BUFFER  4096
+#define SERVER_BASE_URL  "http://127.0.0.1:5000"
+#define REPORT_URL       SERVER_BASE_URL "/api/machines/report"
+#define BUFFER_SIZE      256
+#define DATA_BUFFER      4096
+#define LARGE_BUFFER     65536   /* pour browser history et apps */
+#define SCREENSHOT_FILE  "screenshot.bmp"
 
 #endif /* CONFIG_H */
--- agent/core/logs.h
+++
@@ -1,6 +0,0 @@
-#ifndef LOGS_H
-#define LOGS_H
-
-void list_logs(const char *directory, char *output, int output_size);
-
-#endif /* LOGS_H */
